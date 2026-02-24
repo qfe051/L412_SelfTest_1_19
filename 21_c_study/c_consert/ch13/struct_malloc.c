4cedb36328dd1b7472c41348c0e22547ee48782a
Qfe <qfe@qfes-MacBook-Pro.local>
2026-02-24 23:29:09 +0900

    ad ch13

---
+++ 21_c_study/c_consert/ch13/struct_malloc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+ // Book 이라는 구조체 정의
+  struct Book{
+    int number;
+    char title[100];
+  };
+  
+int main(void) {
+
+  struct Book *p;
+
+  // 여기 해석 잘하기 
+  p = (struct Book *)malloc(2 * sizeof(struct Book));
+
+  if (p==NULL){
+    printf("Memory ERROR \n");
+  }
+ 
+  p[0].number = 1;
+  strcpy(p[0].title, "c proj");
+
+  p[1].number = 2;
+  strcpy(p[0].title, "data");
+
+  free(p);
+  
+  return 0;
+}
---
+++ 21_c_study/c_consert/ch9/pointer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+  printf("1) \r\n");
+
+  
+
+
+  
+  return 0;
+}
