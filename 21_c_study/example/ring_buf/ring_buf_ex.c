d1ac8ef4a6cc112db15947a192e5ce6e698a2dcf
qfe051 <qfe051@cammsys.net>
2026-04-03 18:12:12 +0900

    v260403

--- 21_c_study/example/ring_buf/ring_buf_ex.c
+++ 21_c_study/example/ring_buf/ring_buf_ex.c
@@ -21,12 +21,16 @@ static bool enqueue(ring_st *r,uint8_t rxdata);
 static bool is_full(ring_st *r);
 static uint8_t dequeue(ring_st *r);
 static bool is_empty(ring_st *r);
-static bool display(ring_st *r);
+static bool get_ring_data(ring_st *r, uint8_t *pbuffer);
+static void ring_status(ring_st *r);
+static bool print_temp_data(uint8_t *pbuffer);
 
 
 // 통신용 버퍼 정의
 uint8_t buf_1[BUFFER_SIZE_1];
 uint8_t buf_2[BUFFER_SIZE_2];
+
+uint8_t buf_data_1[BUFFER_SIZE_1];
 
 
 
@@ -45,32 +49,19 @@ int main() {
   enqueue(&ring_1, 20);
   enqueue(&ring_1, 30);
 
+  get_ring_data(&ring_1, buf_data_1);
+  print_temp_data(buf_data_1);
+
   // dequeue 함수 따로 만들기 
   uint8_t temp;
   temp = dequeue(&ring_1);
-  display(&ring_1);
 
-  enqueue(&ring_1, 40);
-  enqueue(&ring_1, 50);
-  enqueue(&ring_1, 60);
-  enqueue(&ring_1, 70);
 
-  display(&ring_1);
 
   //  case2) overflow -> 7개 한번에
   printf("case 2) \n");
   
-  init_ring(&ring_1, buf_1, BUFFER_SIZE_1);
 
-  enqueue(&ring_1, 10);
-  enqueue(&ring_1, 20);
-  enqueue(&ring_1, 30);
-  enqueue(&ring_1, 40);
-  enqueue(&ring_1, 50);
-  enqueue(&ring_1, 60);
-  enqueue(&ring_1, 70);
-
-  display(&ring_1);
 }
 
 //buffer_size에 #define 된 값 넣기 
@@ -83,11 +74,21 @@ void init_ring(ring_st *r,uint8_t *pbuffer ,uint8_t buffer_size) {
 
 
 // 함수 선언 이 아래에 해주기
-bool enqueue(ring_st *r, uint8_t rxdata) {
+bool enqueue_overwrite(ring_st *r, uint8_t rxdata) {
   // is_full 함수 여기 안에 넣기
   if (is_full(r)) {
     // full 일 때 front 덮어쓰기 (밀어내기 후 저장) 옵션
     r->front = (r->front +1)%(r->size);
+  }
+  r->buffer[r->rear] = rxdata;
+  r->rear = (r->rear +1)%(r->size);
+}
+
+bool enqueue(ring_st *r, uint8_t rxdata) {
+  // is_full 함수 여기 안에 넣기
+  if (is_full(r)) {
+    // full 일 때 front 덮어쓰기 (밀어내기 후 저장) 옵션
+    return false;
   }
   r->buffer[r->rear] = rxdata;
   r->rear = (r->rear +1)%(r->size);
@@ -124,19 +125,27 @@ uint8_t dequeue(ring_st *r) {
   return output_data;
 }
 
-bool display(ring_st *r) {
+// 데이터 쌓기용으로 만들기
+bool get_ring_data(ring_st *r,uint8_t *pbuffer) {
+
+    for (int i=0; i<BUFFER_SIZE_1; i++) {
+        pbuffer[i]=0;
+    }
+  
 
   if (is_empty(r)) {
     printf("ring buffer is empty \n");
     return false;
   }
-  
-  printf("[Before]buffer state : size %d | front: %d |rear %d \n", r->size, r->front,
-         r->rear);
+
+
 
   printf("buf data :");
+  // while문 안에 for문. 써도 괜찮은 스타일인지 확인하기 
   while (!is_empty(r)) {
-    printf("[%d]%d ",(r->front+4)%5 ,dequeue(r));
+    for (int i=0; i<BUFFER_SIZE_1; i++) {
+        pbuffer[i]=dequeue(r);
+    }
   }
 
   printf("\n"); 
@@ -144,4 +153,31 @@ bool display(ring_st *r) {
          r->rear);
 
   printf("\n --------------------------------------------------- \n");
+}
+
+void ring_status(ring_st *r) {
+  printf("[Before]buffer state : size %d | front: %d |rear %d \n", r->size,
+         r->front, r->rear);
+  printf("\n --------------------------------------------------- \n");
+}
+
+bool print_temp_data(uint8_t *pbuffer) {
+    if (pbuffer[0]==0) {
+      printf("No temp buff data \n");
+
+      return false;
+    }
+  
+    printf("temp buff data : ");
+
+  for (int i = 0; i < BUFFER_SIZE_1; i++) {
+    if (pbuffer[i] == 0) {
+      printf("\n");
+        return true;
+    }
+    printf("%d ",pbuffer[i]);
+  }
+
+  printf("\n");
+  return true;
 }
