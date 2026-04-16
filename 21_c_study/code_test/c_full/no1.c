8ecac09b5f19277d00af3633790f8c55ab41546a
Qfe <qfe@qfes-MacBook-Pro.local>
2026-04-16 23:15:06 +0900

    no1 동작 확인

--- 21_c_study/code_test/c_full/no1.c
+++ 21_c_study/code_test/c_full/no1.c
@@ -47,9 +47,24 @@ typedef struct gpa_list {
 } gpa_list;
 
 gpa_list s_list[100];
-int list_cnt;
 
-Student *set_student(char *temp_name, char *temp_major, int temp_GPA){
+typedef struct status {
+  int count;
+  int now_process;
+} status;
+
+status sta;
+
+enum S_Process{
+  ADD_STUDENT =1,
+  PRINT_STUDENT_GPA,
+  EXIT
+};
+
+//초기에는 head 값 없음
+Student *head = NULL;
+
+Student *set_student(tmp_data *t){
   Student *s =malloc(sizeof(Student));
   if (s==NULL){
     printf("Student malloc failed\n");
@@ -57,7 +72,7 @@ Student *set_student(char *temp_name, char *temp_major, int temp_GPA){
   }
 
   // 이름 넣기
-  s->name = malloc(strlen(temp_name)+1);
+  s->name = malloc(strlen(t->temp_name)+1);
   if (s->name==NULL){
     printf("s->name malloc failed\n");
     free(s);
@@ -65,7 +80,7 @@ Student *set_student(char *temp_name, char *temp_major, int temp_GPA){
   }
 
   // 전공 넣기
-  s->major = malloc(strlen(temp_major)+1);
+  s->major = malloc(strlen(t->temp_major)+1);
   if (s->major==NULL){
     printf("s->major malloc failed\n");
     free(s);
@@ -73,9 +88,9 @@ Student *set_student(char *temp_name, char *temp_major, int temp_GPA){
     return NULL;
   }
 
-  strcpy(s->name,temp_name);
-  strcpy(s->major,temp_major);
-  s->GPA = temp_GPA;
+  strcpy(s->name,t->temp_name);
+  strcpy(s->major,t->temp_major);
+  s->GPA = t->temp_GPA;
   s->next = NULL;
 
   return s;
@@ -122,27 +137,47 @@ void print_student(const Student *head){
 }
 
 // list 배열 형태로 구조체 선언
-int make_gpa_list(Student *head, gpa_list list[]) {
+bool make_gpa_list(Student *head, gpa_list list[],status *sta) {
+  if (list ==NULL)
+  {
+    printf(">>>>>>>>>>>>>>>>>> ");
+    printf("No list - make_gpa_list \n");
+
+    return false;
+  }
+
+  sta->count=0;
+
   Student *cur = head;
-  int count = 0;
+  
 
   // 순차리스트 출력방식 사용
   while (cur != NULL) {
-    list[count].s_addr = cur;
-    list[count].GPA = cur->GPA;
-    count++;
+    list[sta->count].s_addr = cur;
+    list[sta->count].GPA = cur->GPA;
+    sta->count++;
 
     cur = cur->next;
   }
 
   // 저장 개수
-  return count;
+  return true;
 }
 
 // 정렬 및 출력 모두. 순차리스트와 별개의 인덱스 사용
-void print_gpa_list(gpa_list list[], int count) {
-  for (int j = 0; j < count - 1; j++) {
-    for (int i = 0; i < count - 1; i++) {
+bool print_gpa_list(gpa_list list[], status *sta) {
+  if (list ==NULL || sta->count==0)
+  {
+    printf(">>>>>>>>>>>>>>>>>> ");
+    printf("No data - print_gpa_list \n");
+
+    return false;
+  }
+  
+
+
+  for (int j = 0; j < sta->count - 1; j++) {
+    for (int i = 0; i < sta->count - 1; i++) {
       if (list[i].GPA < list[i + 1].GPA) {
         gpa_list tmp = list[i];
         list[i] = list[i + 1];
@@ -151,13 +186,15 @@ void print_gpa_list(gpa_list list[], int count) {
     }
   }
 
-  for (int j = 0; j < count; j++) {
+  for (int j = 0; j < sta->count; j++) {
     printf("---------------------------------------------- \n");
     printf("index : %d \n", j);
     printf("name : %s \n", list[j].s_addr->name);
     printf("major : %s \n", list[j].s_addr->major);
     printf("GPA : %d \n", list[j].s_addr->GPA);
   }
+
+  return true;
 }
 
 //free 과정 다시보기 
@@ -176,47 +213,82 @@ void free_all(Student **head){
   *head = NULL;
 }
 
+bool process_input(tmp_data *t,status *sta){
+  if (t==NULL || sta ==NULL){
+    printf(">>>>>>>>>>>>>>>>>> ");
+    printf("No data - process_input \n");
+  }
+  
+
+  // while (!(t->type_input==1||t->type_input==2||t->type_input==3)){}
+    t->type_input = 0;
+    printf("type (1) : add student \n type (2) : print student \n type (3) : "
+            "exit \n");
+    scanf("%d", &t->type_input);
+  
+  
+  sta->now_process=t->type_input;
+
+  switch (sta->now_process)
+  {
+  case ADD_STUDENT:
+    printf("type name : ");
+    scanf("%s", t->temp_name);
+
+    printf("type major : ");
+    scanf("%s", t->temp_major);
+
+    printf("type GPA : ");
+    scanf("%d", &t->temp_GPA);
+
+    Student *s_1 = set_student(t);
+
+    printf("append_student \n ");
+    // append 함수 내에 정렬까지 추가하기
+    append_student(&head, s_1);
+
+    printf("print_student \n ");
+    print_student(head);
+
+    break;
+  case PRINT_STUDENT_GPA:
+    //if문으로 함수 true일 때 실행
+    if (make_gpa_list(head, s_list,sta))
+      {
+        printf("make_gpa_list is done \n\n ");
+        if (print_gpa_list(s_list, sta))
+        {
+          printf("print_gpa_list is done \n\n ");
+        }
+      }
+      break;
+  case EXIT:
+    printf("process is done \n\n ");
+    free_all(&head);
+    break;
+  default:
+    printf("type again \n ");
+    return false;
+    break;
+  }
+
+}
+
 int main() {
-  //초기에는 head 값 없음
-  Student *head = NULL;
+  
+while(1){
+    while (process_input(&t,&sta))
+    {
+      
+    }
 
-  while (1) {
-    t.type_input = 0;
-    printf("type (1) : add student, type (2) : print student,type others : "
-           "exit \n");
-    scanf("%d", &t.type_input);
+    if (sta.now_process==EXIT)
+    {
+      printf("process is done ,.......... \n\n ");
 
-    if (t.type_input == 1) {
-      printf("type name : ");
-      scanf("%s", t.temp_name);
-
-      printf("type major : ");
-      scanf("%s", t.temp_major);
-
-      printf("type GPA : ");
-      scanf("%d", &t.temp_GPA);
-
-      printf("name : %s , major : %s , GPA : %d \n", t.temp_name, t.temp_major,
-             t.temp_GPA);
-
-      printf("temp_GPA : %d \n", t.temp_GPA);
-
-      Student *s_1 = set_student(t.temp_name, t.temp_major, t.temp_GPA);
-
-      printf("append_student \n ");
-      // append 함수 내에 정렬까지 추가하기
-      append_student(&head, s_1);
-
-      printf("print_student \n ");
-      print_student(head);
-    } else if (t.type_input == 2) {
-      printf("make_gpa_list \n ");
-      list_cnt = make_gpa_list(head, s_list);
-      printf("print_gpa_list \n ");
-      print_gpa_list(s_list, list_cnt);
-    } else {
-      free_all(&head);
       return 0;
     }
+    
+
   }
 }
